9b99ab592dcefde2b191380428201e4424df9d01
Kris Kurzawa <kurzawa1@yahoo.com>
2023-08-28 22:30:27 -0400

    updated routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,11 +9,11 @@ router.get('/', async (req, res) => {
     // be sure to include its associated Category and Tag data
     try {
         const productData = await Product.findAll({
-            include: [{ model: Category }, { model: Tag }, { model: ProductTag }],
+            include: [{ model: Category }, { model: Tag }],
         });
         res.status(200).json(productData);
     } catch (err) {
-        res.status.json(err);
+        res.status(500).json(err);
     }
 });
 
